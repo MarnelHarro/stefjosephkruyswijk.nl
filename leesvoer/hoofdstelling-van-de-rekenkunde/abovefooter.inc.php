501ee3027ecc03f0d52205cd30a20a5ba72a343f
Marnel Harro <marnelharro@tutanota.com>
2020-10-20 16:35:50 +0200

    hoofdstelling van de rekenkunde source & h1 line-height  update

--- leesvoer/hoofdstelling-van-de-rekenkunde/abovefooter.inc.php
+++ leesvoer/hoofdstelling-van-de-rekenkunde/abovefooter.inc.php
@@ -1,4 +1,10 @@
-	
+
+		<div class="displaygrid">
+			<h3>Bronnen</h3>
+			<ul style="margin-top: -10px">
+				<li>Weisstein, Eric W., Fundamental Theorem of Arithmetic, From <a href='https://mathworld.wolfram.com/FundamentalTheoremofArithmetic.html' target='_blank'>MathWorld--A Wolfram Web Resource.</a></li>
+			</ul>
+		</div>
 
 	</body>
 
